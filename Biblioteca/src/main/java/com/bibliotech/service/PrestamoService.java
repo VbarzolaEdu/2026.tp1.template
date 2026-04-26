1b20bbd57d54bd77f1a3403a138b35e73fdced58
Valentin <valentinbarzola@gmail.com>
2026-04-26 20:06:01 -0300

    service para prestamo

---
+++ Biblioteca/src/main/java/com/bibliotech/service/PrestamoService.java
@@ -0,0 +1,68 @@
+package com.bibliotech.service;
+
+import com.bibliotech.model.*;
+import com.bibliotech.repository.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class PrestamoService {
+
+    private final Repository<Libro, String> libroRepo;
+    private final Repository<Socio, Integer> socioRepo;
+    private final Repository<Prestamo, String> prestamoRepo;
+
+    public PrestamoService(
+            Repository<Libro, String> libroRepo,
+            Repository<Socio, Integer> socioRepo,
+            Repository<Prestamo, String> prestamoRepo) {
+
+        this.libroRepo = libroRepo;
+        this.socioRepo = socioRepo;
+        this.prestamoRepo = prestamoRepo;
+    }
+
+    public void realizarPrestamo(String isbn, int socioId) {
+
+        // 1. validar libro
+        Libro libro = libroRepo.buscarPorId(isbn)
+                .orElseThrow(() -> new RuntimeException("Libro no encontrado"));
+
+        // 2. validar socio
+        Socio socio = socioRepo.buscarPorId(socioId)
+                .orElseThrow(() -> new RuntimeException("Socio no encontrado"));
+
+        // 3. validar disponibilidad
+        boolean prestado = prestamoRepo.buscarTodos().stream()
+                .anyMatch(p ->
+                        p.isbnLibro().equals(isbn) &&
+                                p.fechaDevolucion().isEmpty()
+                );
+
+        if (prestado) {
+            throw new RuntimeException("Libro ya prestado");
+        }
+
+        // 4. validar límite
+        long prestamosActivos = prestamoRepo.buscarTodos().stream()
+                .filter(p ->
+                        p.socioId() == socioId &&
+                                p.fechaDevolucion().isEmpty()
+                )
+                .count();
+
+        if (prestamosActivos >= socio.limitePrestamos()) {
+            throw new RuntimeException("Límite de préstamos alcanzado");
+        }
+
+        // 5. crear préstamo
+        Prestamo prestamo = new Prestamo(
+                isbn,
+                socioId,
+                LocalDate.now(),
+                Optional.empty()
+        );
+
+        prestamoRepo.guardar(prestamo);
+    }
+}
